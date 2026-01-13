7d5d8461845acdd5d2317b5c3adceba1db055d44
Luke Bakken <lrbakken@amazon.com>
2026-01-13 16:47:27 +0000

    Add compatibility check route and formatters to queue-migration.js
    - Add POST route for #/queue-migration/check
    - Add formatter functions: fmt_check_type_name, fmt_compatibility_status,
      fmt_issue_type, fmt_system_check_status, toggle_issue_details
    - Results render into #compatibility-results div (to be added to template)

--- priv/www/js/queue-migration.js
+++ priv/www/js/queue-migration.js
@@ -14,6 +14,23 @@ dispatcher_add(function(sammy) {
                'queue-migration-detail-status', '#/queue-migration/status');
     });
 
+    sammy.post('#/queue-migration/check', function() {
+        var self = this;
+        var vhost = self.params.vhost || '/';
+        var requestBody = {};
+        if (self.params.skip_unsuitable_queues === 'on') {
+            requestBody.skip_unsuitable_queues = true;
+        }
+
+        with_req('POST', '/queue-migration/check/' + encodeURIComponent(vhost), JSON.stringify(requestBody), function(resp) {
+            var data = JSON.parse(resp.responseText);
+            var html = format('queue-migration-check-results', {compatibility_results: data});
+            $('#compatibility-results').html(html);
+        });
+
+        return false;
+    });
+
     sammy.put('#/queue-migration/start', function() {
         var self = this;
 
@@ -101,4 +118,49 @@ function fmt_sort_desc_by_default(display, sort) {
             '</span>';
     }
     return '<a class="sort" sort="' + sort + '">' + prefix + display + '</a>';
+}
+
+// Compatibility check formatters
+
+function fmt_check_type_name(checkType) {
+    var nameMap = {
+        'relaxed_checks_setting': 'Relaxed Checks Setting',
+        'leader_balance': 'Queue Leader Balance',
+        'queue_synchronization': 'Queue Synchronization',
+        'queue_suitability': 'Queue Suitability',
+        'message_count': 'Message Count Limits',
+        'disk_space': 'Disk Space'
+    };
+    return nameMap[checkType] || checkType;
+}
+
+function fmt_compatibility_status(compatible) {
+    return compatible ?
+        '<span class="status-green">Compatible</span>' :
+        '<span class="status-red">Unsuitable</span>';
+}
+
+function fmt_issue_type(type) {
+    var typeMap = {
+        'exclusive': 'Exclusive Queue',
+        'unsupported_argument': 'Unsupported Argument',
+        'max_priority': 'Priority Queue',
+        'lazy_mode': 'Lazy Mode',
+        'overflow_behavior': 'Overflow Behavior',
+        'unsuitable_overflow': 'Unsuitable Overflow',
+        'message_count_limit': 'Too Many Messages',
+        'data_size_limit': 'Too Much Data',
+        'too_many_queues': 'Too Many Queues'
+    };
+    return typeMap[type] || type;
+}
+
+function fmt_system_check_status(status) {
+    return status === 'passed' ?
+        '<span class="status-green">✓ Passed</span>' :
+        '<span class="status-red">✗ Failed</span>';
+}
+
+function toggle_issue_details(queueName) {
+    $('#issues-' + queueName).toggle();
 }
